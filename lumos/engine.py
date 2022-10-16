e8060807d3c7a1744f878e2eee280d70ffdafc8f
harley-zhang <harleyzgolego@gmail.com>
2022-10-16 10:00:00 -0400

    few-shot prompt for suggest

--- lumos/engine.py
+++ lumos/engine.py
@@ -2,3 +2,26 @@ import os
 import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+def _suggest_prompt(description):
+    return f"""Translate the description into a single shell command.
+
+Description: list all files sorted by size
+Command: ls -lhS
+
+Description: find python files modified in the last week
+Command: find . -name "*.py" -mtime -7
+
+Description: {description}
+Command:"""
+
+
+def suggest(description):
+    response = openai.Completion.create(
+        model="davinci-002",
+        prompt=_suggest_prompt(description),
+        max_tokens=150,
+        temperature=0,
+    )
+    return response.choices[0].text.strip()
