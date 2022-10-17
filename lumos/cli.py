a117b1db7c74dbd9852081005bab6cd87bf1a4b5
harley-zhang <harleyzgolego@gmail.com>
2022-10-17 09:15:00 -0400

    rich panel output

--- lumos/cli.py
+++ lumos/cli.py
@@ -1,8 +1,18 @@
 import click
+from rich.console import Console
+from rich.panel import Panel
+from rich.text import Text
 from lumos.engine import suggest
+
+console = Console()
 
 @click.command()
 @click.argument("description")
 def cli(description):
     command = suggest(description)
-    click.echo(command)
+    text = Text()
+    text.append("$ ", style="green bold")
+    text.append(command)
+    console.print(
+        Panel(text, title="lumos", title_align="left", border_style="blue", padding=(1, 2))
+    )
