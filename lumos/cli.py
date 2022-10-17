b779e04f6d45b1a5e0699f6d3786040bdb56cd98
harley-zhang <harleyzgolego@gmail.com>
2022-10-17 14:20:00 -0400

    handle missing api key

--- lumos/cli.py
+++ lumos/cli.py
@@ -5,7 +5,7 @@ import click
 from rich.console import Console
 from rich.panel import Panel
 from rich.text import Text
-from lumos.engine import suggest
+from lumos.engine import suggest, explain
 
 console = Console()
 
@@ -18,15 +18,33 @@ def _display_command(command):
     )
 
 @click.command()
-@click.argument("description")
-def cli(description):
-    with console.status("thinking...", spinner="dots"):
-        command = suggest(description)
-    _display_command(command)
-    should_run = console.input(
-        "\n  run this command? [dim]\\[y/n][/dim] "
-    ).strip().lower() == "y"
-    if should_run:
-        console.print()
-        result = subprocess.run(command, shell=True)
-        sys.exit(result.returncode)
+@click.argument("input_text", required=False)
+@click.option("-e", "--explain", "explain_mode", is_flag=True)
+def cli(input_text, explain_mode):
+    if explain_mode:
+        if not input_text:
+            console.print("  provide a command to explain", style="red")
+            raise SystemExit(1)
+        with console.status("thinking...", spinner="dots"):
+            explanation = explain(input_text)
+        text = Text()
+        text.append("$ ", style="green bold")
+        text.append(input_text)
+        text.append("\n\n")
+        text.append(explanation, style="dim")
+        console.print(
+            Panel(text, title="lumos", title_align="left", border_style="blue", padding=(1, 2))
+        )
+    elif input_text:
+        with console.status("thinking...", spinner="dots"):
+            command = suggest(input_text)
+        _display_command(command)
+        should_run = console.input(
+            "\n  run this command? [dim]\\[y/n][/dim] "
+        ).strip().lower() == "y"
+        if should_run:
+            console.print()
+            result = subprocess.run(command, shell=True)
+            sys.exit(result.returncode)
+    else:
+        click.echo(click.get_current_context().get_help())
