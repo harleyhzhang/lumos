ebcd55d8aa07fe3b59da352bf7c3ca6b549335b6
harley-zhang <harleyzgolego@gmail.com>
2022-10-18 12:10:00 -0400

    fix timestamp format

--- lumos/history.py
+++ lumos/history.py
@@ -4,24 +4,29 @@ from pathlib import Path
 
 HISTORY_PATH = Path.home() / ".lumos_history.json"
 
+
 def _load():
     if not HISTORY_PATH.exists():
         return []
     with open(HISTORY_PATH) as f:
         return json.load(f)
 
+
 def _save(entries):
     with open(HISTORY_PATH, "w") as f:
         json.dump(entries, f, indent=2)
 
-def save_entry(description, command):
+
+def save_entry(description, command, executed=False):
     entries = _load()
     entries.append({
         "description": description,
         "command": command,
+        "executed": executed,
         "timestamp": datetime.now().isoformat(),
     })
     _save(entries)
+
 
 def get_recent(n=20):
     return _load()[-n:]
