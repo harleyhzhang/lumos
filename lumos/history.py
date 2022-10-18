0770f8fba1fcf027d62229d370452cbdabc09599
harley-zhang <harleyzgolego@gmail.com>
2022-10-18 09:30:00 -0400

    add history module

--- lumos/history.py
+++ lumos/history.py
@@ -4,29 +4,24 @@ from pathlib import Path
 
 HISTORY_PATH = Path.home() / ".lumos_history.json"
 
-
-def _load() -> list[dict]:
+def _load():
     if not HISTORY_PATH.exists():
         return []
     with open(HISTORY_PATH) as f:
         return json.load(f)
 
-
-def _save(entries: list[dict]):
+def _save(entries):
     with open(HISTORY_PATH, "w") as f:
         json.dump(entries, f, indent=2)
 
-
-def save_entry(description: str, command: str, executed: bool = False):
+def save_entry(description, command):
     entries = _load()
     entries.append({
         "description": description,
         "command": command,
-        "executed": executed,
         "timestamp": datetime.now().isoformat(),
     })
     _save(entries)
 
-
-def get_recent(n: int = 20) -> list[dict]:
+def get_recent(n=20):
     return _load()[-n:]
