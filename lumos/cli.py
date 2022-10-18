c21a0a6096a60c87ff94813809be16082c2175b0
harley-zhang <harleyzgolego@gmail.com>
2022-10-18 11:45:00 -0400

    history table with rich

--- lumos/cli.py
+++ lumos/cli.py
@@ -4,9 +4,10 @@ import sys
 import click
 from rich.console import Console
 from rich.panel import Panel
+from rich.table import Table
 from rich.text import Text
 from lumos.engine import suggest, explain
-from lumos.history import save_entry
+from lumos.history import save_entry, get_recent
 
 console = Console()
 
@@ -18,11 +19,29 @@ def _display_command(command):
         Panel(text, title="lumos", title_align="left", border_style="blue", padding=(1, 2))
     )
 
+def _history(limit):
+    entries = get_recent(limit)
+    if not entries:
+        console.print("  no history yet", style="dim")
+        return
+    table = Table(title="history", border_style="blue", show_header=True, header_style="bold")
+    table.add_column("time", style="dim")
+    table.add_column("description")
+    table.add_column("command", style="green")
+    for entry in entries:
+        ts = entry["timestamp"][:16].replace("T", " ")
+        table.add_row(ts, entry["description"], entry["command"])
+    console.print(table)
+
 @click.command()
 @click.argument("input_text", required=False)
 @click.option("-e", "--explain", "explain_mode", is_flag=True)
-def cli(input_text, explain_mode):
-    if explain_mode:
+@click.option("-H", "--history", "show_history", is_flag=True)
+@click.option("-n", "--limit", default=20)
+def cli(input_text, explain_mode, show_history, limit):
+    if show_history:
+        _history(limit)
+    elif explain_mode:
         if not input_text:
             console.print("  provide a command to explain", style="red")
             raise SystemExit(1)
